d26cf167cdb4c8813912a413bb579afa0bdc8b1b
James Donlan <jacketlizard@live.com>
2018-01-12 15:01:45 -0800

    solved internal server error, teams can now be created and listed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
+import models.Team;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,5 +18,28 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
+        //get: show new team form
+        get("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //post: process new team form
+        post("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            String description = request.queryParams("description");
+            Team newTeam = new Team(name, description);
+            model.put("team", newTeam);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show all teams
+        get("/teams", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Team> teams = Team.getAll();
+            model.put("teams", teams);
+            return new ModelAndView(model, "show-all.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
